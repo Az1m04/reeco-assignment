71b6d7ec179c336dc16db9b5788a1f8cf44bbe78
Md Azim Babu <42888008+Az1m04@users.noreply.github.com>
2023-09-27 19:47:20 +0530

    updated Readme

--- reeco-assignment/src/ReduxStore/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import OrderSlice from "./Slices/OrderSlice";
-
-
-export const store = configureStore({
-    reducer: {
-        orders: OrderSlice,
-    }
-});
